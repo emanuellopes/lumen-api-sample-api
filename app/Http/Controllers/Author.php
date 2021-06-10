d34d6f191ba7e4f867469b1345bafa0a78db5f35
Emanuel Lopes <emanuel.lopes@namecheap.com>
2021-06-10 15:09:25 +0100

    fix: added controller method for save, update and get single author

--- app/Http/Controllers/Author.php
+++ app/Http/Controllers/Author.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -54,17 +55,48 @@ class Author extends Controller
     /**
      * obtains and show one author
      */
-    public function show(int $id): Response
+    public function show(int $id): JsonResponse
     {
+        try {
+            $author = AuthorModel::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_NOT_FOUND);
+        }
 
+        return $this->successResponse($author);
     }
 
     /**
      * update one author
      */
-    public function update(Request $request, int $id): Response
+    public function update(Request $request, int $id): JsonResponse
     {
+        $rules = [
+            'name'    => 'max:255|string',
+            'gender'  => 'max:255|in:male,female',
+            'country' => 'size:2|string|in:PT,US,CH',
+        ];
 
+        try {
+            $this->validate($request, $rules);
+            $author = AuthorModel::findOrFail($id);
+        } catch (ValidationException $e) {
+            return $this->errorResponse($e->errors(), Response::HTTP_BAD_REQUEST);
+        } catch (ModelNotFoundException $e) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_NOT_FOUND);
+        }
+
+        $author->fill($request->all());
+
+        if ($author->isClean()) {
+            return $this->errorResponse("As leat one value must change", Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ( ! $author->save()) {
+            return $this->errorResponse("User not saved", Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return $this->successResponse($author);
     }
 
 
--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,10 +5,33 @@ namespace App\Models;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
+/**
+ * Class Author
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ * @package App\Models
+ * @property int $id
+ * @property string $name
+ * @property string $gender
+ * @property string $country
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\AuthorFactory factory(...$parameters)
+ * @method static Builder|Author newModelQuery()
+ * @method static Builder|Author newQuery()
+ * @method static Builder|Author query()
+ * @method static Builder|Author whereCountry($value)
+ * @method static Builder|Author whereCreatedAt($value)
+ * @method static Builder|Author whereGender($value)
+ * @method static Builder|Author whereId($value)
+ * @method static Builder|Author whereName($value)
+ * @method static Builder|Author whereUpdatedAt($value)
+ */
 class Author extends Model
 {
     use Authenticatable, Authorizable, HasFactory;
